d57c87f57db10795b0e9cb4aecc1b7acf884a97d
Andrew Owen <47571927+Andr3w0w3n@users.noreply.github.com>
2023-03-23 23:29:44 -0600

    update code to get closer to completion

--- CopyCatCreator.py
+++ CopyCatCreator.py
@@ -1,10 +1,13 @@
 import nuke
 
 frame = [0, 15, 30, 45, 60]
-frameRange = nuke.nodes.FrameRange(first_frame= 1, last_frame= 1)
+frameRange = nuke.nodes.FrameRange(first_frame = 1, last_frame = 1)
 
 def createFrameholds():
     for x in frame:
-        break
+        fh = nuke.nodes.FrameHold(firstFrame = x).setInput(0, frameRange)
+        nuke.nodes.Roto(premultiply = "alpha").setInput(0, fh)
+
+frameRange.setInput(0, selectedNode())
 
 
