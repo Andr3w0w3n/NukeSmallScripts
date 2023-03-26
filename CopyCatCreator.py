396ff35d3c245abe2e9a3144f41d458235660663
Andrew Owen <47571927+Andr3w0w3n@users.noreply.github.com>
2023-03-25 22:43:51 -0600

    Script works!
    I had to trade out the shuffle node for the copy node, there is no real difference but I am frustrated I was unable to figure out the error for the shuffle node

--- CopyCatCreator.py
+++ CopyCatCreator.py
@@ -8,8 +8,10 @@ rotos = []
 #creating the frame holds and rotos
 def createFrameholds():
     for x in frame:
-        fh = nuke.nodes.FrameHold(firstFrame = frame[x]).setInput(0, frameRange)
-        roto = nuke.nodes.Roto(output = "alpha", premultiply = "alpha").setInput(0, fh)
+        fh = nuke.nodes.FrameHold(firstFrame = x)
+        fh.setInput(0, frameRange)
+        roto = nuke.nodes.Roto(output = "alpha", premultiply = "alpha")
+        roto.setInput(0, fh)
         rotos.append(roto)
         
 
@@ -17,7 +19,24 @@ def createFrameholds():
 frameRange.setInput(0, nuke.selectedNode())
 createFrameholds()
 appendClips = nuke.nodes.AppendClip(inputs = rotos)
-shuffleGround = nuke.nodes.Shuffle(inputs = [appendClips])
-removeGround = nuke.nodes.Remove(operation = "keep", channels = "red").setInput(0, appendClips)
+
+#setting ground truth
+
+#WTF, WHY WONT THIS SHUFFLE WORK??? ChatGPT can't even seem to come up with a correct answer!!!!
+#*shuffleGround = nuke.createNode('Shuffle')
+#shuffleGround.setInput(0, appendClips)
+#shuffleGround.knob('red_in').setValue('alpha')
+
+copyGround = nuke.nodes.Copy(input = [appendClips, appendClips])
+copyGround.knob('to0').setValue('rgba.red')
 
 
+removeGround = nuke.nodes.Remove(operation = "keep", channels = "red")
+removeGround.setInput(0, copyGround)
+
+#setting input
+removeInput = nuke.nodes.Remove(operation = "keep", channels = "rgb")
+
+#copycat
+
+copyCatNode = nuke.nodes.CopyCat(inputs = [removeInput, removeGround])
